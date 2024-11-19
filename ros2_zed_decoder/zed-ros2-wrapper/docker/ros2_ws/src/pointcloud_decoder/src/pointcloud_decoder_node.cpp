867b7fe20ce941b0a61218c2b0fcba78b9827824
TheLiukas <max@MaxLaptop>
2024-11-19 16:15:21 +0100

    Creato pkg per decoding della compressed point cloud

--- ros2_zed_decoder/zed-ros2-wrapper/docker/ros2_ws/src/pointcloud_decoder/src/pointcloud_decoder_node.cpp
+++ ros2_zed_decoder/zed-ros2-wrapper/docker/ros2_ws/src/pointcloud_decoder/src/pointcloud_decoder_node.cpp
@@ -29,27 +29,91 @@ private:
         buffer.Init(reinterpret_cast<const char *>(msg->compressed_data.data()), msg->compressed_data.size());
 
         // Call the decoder
-        auto result = decoder.DecodePointCloudFromBuffer(&buffer);
+       auto result = decoder.DecodePointCloudFromBuffer(&buffer);
         if (!result.ok()) {
             RCLCPP_ERROR(this->get_logger(), "Failed to decode point cloud: %s", result.status().error_msg());
             return;
         }
 
         // Extract the decoded point cloud
-        std::unique_ptr<draco::PointCloud> decoded_cloud = std::move(result).value();
+        const std::unique_ptr<draco::PointCloud> & decoded_pc = result.value();
+        
         RCLCPP_INFO(this->get_logger(), "Decoded point cloud successfully");
-
+	
         // Convert the Draco PointCloud to ROS PointCloud2 (this requires custom conversion logic)
         sensor_msgs::msg::PointCloud2 output_msg;
+        convertDracoToPC2(*decoded_pc,*msg,output_msg);
         // output_msg.data="prova";
         // Conversion logic goes here to populate `output_msg` from `decoded_cloud`
         publisher_->publish(output_msg);
     }
+    //! Method for converting into sensor_msgs::msg::PointCloud2
+	bool convertDracoToPC2(
+	  const draco::PointCloud & pc,
+	  const point_cloud_interfaces::msg::CompressedPointCloud2 & compressed_PC2,
+	  sensor_msgs::msg::PointCloud2 & PC2)
+	{
+	  // number of all attributes of point cloud
+	  int32_t number_of_attributes = pc.num_attributes();
+
+	  // number of points in pointcloud
+	  draco::PointIndex::ValueType number_of_points = pc.num_points();
+
+	  PC2.data.resize(number_of_points * compressed_PC2.point_step);
+
+	  // for each attribute
+	  for (int32_t att_id = 0; att_id < number_of_attributes; att_id++) {
+	    // get attribute
+	    const draco::PointAttribute * attribute = pc.attribute(att_id);
+
+	    // check if attribute is valid
+	    if (!attribute->IsValid()) {
+		RCLCPP_ERROR(this->get_logger(), "In point_cloud_transport::DracoToPC2, attribute of Draco pointcloud is not valid!");
+            	return false;
+	    }
+
+	    // get offset of attribute in data structure
+	    uint32_t attribute_offset = compressed_PC2.fields[att_id].offset;
+
+	    // for each point in point cloud
+	    for (draco::PointIndex::ValueType point_index = 0; point_index < number_of_points;
+	      point_index++)
+	    {
+	      // get pointer to corresponding memory in PointCloud2 data
+	      uint8_t * out_data =
+		&PC2.data[static_cast<int>(compressed_PC2.point_step * point_index + attribute_offset)];
+
+	      // read value from Draco pointcloud to out_data
+	      attribute->GetValue(draco::AttributeValueIndex(point_index), out_data);
+	    }
+	  }
+
+	  // copy PointCloud2 description (header, width, ...)
+	  copyCloudMetadata(PC2, compressed_PC2);
+
+	  return true;
+	}
+	 void copyCloudMetadata(sensor_msgs::msg::PointCloud2 & target, const point_cloud_interfaces::msg::CompressedPointCloud2 & source)
+	  {
+	    target.header = source.header;
+	    target.height = source.height;
+	    target.width = source.width;
+	    target.fields = source.fields;
+	    target.is_bigendian = source.is_bigendian;
+	    target.point_step = source.point_step;
+	    target.row_step = source.row_step;
+	    target.is_dense = source.is_dense;
+	  }
 
     // Member variables for subscription and publisher
     rclcpp::Subscription<point_cloud_interfaces::msg::CompressedPointCloud2>::SharedPtr subscription_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
 };
+
+
+
+
+
 
 // Main function
 int main(int argc, char **argv) {

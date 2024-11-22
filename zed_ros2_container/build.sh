0514aa809daa207cbd8e51082cebc52629b37c79
TheLiukas <max@MaxLaptop>
2024-11-22 12:50:45 +0100

    Corretto calibration file per zed

--- zed_ros2_container/build.sh
+++ zed_ros2_container/build.sh
@@ -7,3 +7,4 @@ ls
 sudo chmod 777 desktop_build_dockerfile_from_sdk_ubuntu_and_cuda_version.sh
 sudo ./desktop_build_dockerfile_from_sdk_ubuntu_and_cuda_version.sh ubuntu22.04 cuda12.1.0 zedsdk4.1.4
 cd ../..
+pwd

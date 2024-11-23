cee748f3b39dcb85b9d227c37740c66b5b07b9be
TheLiukas <max@MaxLaptop>
2024-11-23 13:00:38 +0100

    Risolto problema dipendenza zed_interfaces; aggiunto hostname per container; corretto Dockerfile

--- ros2_rviz_container/run.sh
+++ ros2_rviz_container/run.sh
@@ -1,3 +1,3 @@
 xhost +
-sudo docker run --rm -it --net host --ipc host     -e DISPLAY     -v /tmp/.X11-unix/:/tmp/.X11-unix     -v ~/.Xauthority:/root/.Xauthority -v ./ros2bag:/root/home/ros2bag    -e XAUTHORITY=/root/.Xauthority     ros2-rviz bash
+sudo docker run --rm -it --net host --ipc host --hostname ros2rvizcontainer -e DISPLAY     -v /tmp/.X11-unix/:/tmp/.X11-unix     -v ~/.Xauthority:/root/.Xauthority -v ./ros2bag:/root/home/ros2bag    -e XAUTHORITY=/root/.Xauthority     ros2-rviz bash
  
